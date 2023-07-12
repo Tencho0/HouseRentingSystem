1fb41852d30a0af1cfd92cf4f1fd1e8fc30c5f25
Tencho <tencho1011@gmail.com>
2023-07-12 22:48:41 +0300

    Added Common project

--- HouseRentingSystem.Web/Controllers/HomeController.cs
+++ HouseRentingSystem.Web/Controllers/HomeController.cs
@@ -1,28 +1,20 @@
 ﻿namespace HouseRentingSystem.Web.Controllers
 {
     using System.Diagnostics;
-    using HouseRentingSystem.Web.Models;
     using Microsoft.AspNetCore.Mvc;
+
+    using Models;
 
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public HomeController()
+        { }
 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

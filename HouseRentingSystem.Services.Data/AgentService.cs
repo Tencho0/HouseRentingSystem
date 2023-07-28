0de943aff38410c7f9165272cf2da33539801de7
Tencho <tencho1011@gmail.com>
2023-07-28 09:10:55 +0300

    Implemented Become Agent

---
+++ HouseRentingSystem.Services.Data/AgentService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Services.Data
+{
+    using System.Threading.Tasks;
+    using HouseRentingSystem.Data;
+    using HouseRentingSystem.Services.Data.Interfaces;
+
+    public class AgentService : IAgentService
+    {
+        private readonly HouseRentingDbContext dbContext;
+
+        public AgentService(HouseRentingDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<bool> AgentExistsByUserIdAsync(string userId)
+        {
+            bool result = await dbContext.Agents
+                .AnyAsync(a => a.UserId.ToString() == userId);
+
+            return result;
+        }
+    }
+}
--- HouseRentingSystem.Web/Controllers/AgentController.cs
+++ HouseRentingSystem.Web/Controllers/AgentController.cs
@@ -1,13 +1,35 @@
 ﻿namespace HouseRentingSystem.Web.Controllers
 {
-    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Infrastructure.Extensions;
+    using HouseRentingSystem.Services.Data.Interfaces;
+
+    using static Common.NotificationMessagesConstants;
 
     [Authorize]
     public class AgentController : Controller
     {
-        public async Task<IActionResult> Index()
+        private readonly IAgentService agentService;
+
+        public AgentController(IAgentService agentService)
         {
+            this.agentService = agentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Become()
+        {
+            string? userId = User.GetId();
+            bool isAgent = await agentService.AgentExistsByUserIdAsync(userId!);
+            if (isAgent)
+            {
+                TempData[ErrorMessage] = "You are already an agent!";
+
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
         }
     }
